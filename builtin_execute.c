296c5a29124cbc0dfa7881b0fe09784c1ec9d7b8
hamza-essalhi <hamzaessalhix@gmail.com>
2023-05-26 16:13:36 -0700

    fexing

--- builtin_execute.c
+++ builtin_execute.c
@@ -2,12 +2,13 @@
 
 /**
  * builtin_execute - execute in child process
+ * @count_command: coount the command
  * @arguments: String of arguments
  * @argv: Array of arguments
  * @exit_status: Exit status
  * Return: return 1
  */
-int builtin_execute(char **arguments, char **argv, 
+int builtin_execute(char **arguments, char **argv,
 		int *exit_status, int count_command)
 {
 	pid_t pid_used;
